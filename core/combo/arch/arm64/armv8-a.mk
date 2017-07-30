4ed6febc2db84405a98fab71384661aa720e845c
Alex Naidis <alex.naidis@linux.com>
2017-07-30 03:27:26 +0000

    build: condition A53 errata workarounds in RenderScript
    Change-Id: I3bc8b4ae421455aa6e70219c465950462aca6f40
    Signed-off-by: DarkKnight6499 <yazad.madan@gmail.com>

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -2,4 +2,6 @@ ifneq (,$(filter cortex-a53,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
 	arch_variant_cflags := -mcpu=cortex-a53
 else
 	arch_variant_cflags :=
+	RS_DISABLE_A53_WORKAROUND := true
 endif
+
