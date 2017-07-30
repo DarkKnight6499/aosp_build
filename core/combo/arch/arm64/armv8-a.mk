0c5f8610633f2e41bb5f07102184cff94911b1d2
Alex Naidis <alex.naidis@linux.com>
2017-07-30 03:32:09 +0000

    build: take ARM erratum cortex-a53-835769 into our hands
    Change-Id: If503fbae1d465f015a4d77d40da78d74b3530157
    Signed-off-by: DarkKnight6499 <yazad.madan@gmail.com>

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -1,9 +1,11 @@
 ifneq (,$(filter cortex-a53,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
 	arch_variant_cflags := -mcpu=cortex-a53
 	arch_variant_ldflags += -Wl,--fix-cortex-a53-835769
+	arch_variant_cflags  += -mfix-cortex-a53-835769
 else
 	arch_variant_cflags :=
 	arch_variant_ldflags += -Wl,--no-fix-cortex-a53-835769
+	arch_variant_cflags  += -mno-fix-cortex-a53-835769
 	RS_DISABLE_A53_WORKAROUND := true
 endif
 
