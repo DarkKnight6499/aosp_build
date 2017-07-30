e0881a14466f5728645b3c21cbd0c534b90262a7
Alex Naidis <alex.naidis@linux.com>
2017-07-30 03:38:50 +0000

    arm64: Rework A53 Errata Fixing logic
    Change-Id: Ie87ceba5bb4a5d028463ea38ff52cc5dfc94e698
    Signed-off-by: DarkKnight6499 <yazad.madan@gmail.com>

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -1,4 +1,10 @@
-ifneq (,$(filter cortex-a53,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+APPLY_A53_ERRATA_FIXES :=
+
+ifneq ($(strip $(TARGET_IS_CORTEX-A53)),)
+	APPLY_A53_ERRATA_FIXES := $(TARGET_IS_CORTEX-A53)
+endif
+
+ifeq ($(APPLY_A53_ERRATA_FIXES),true)
 	arch_variant_cflags := -mcpu=cortex-a53
 	arch_variant_ldflags += -Wl,--fix-cortex-a53-835769
 	arch_variant_cflags  += -mfix-cortex-a53-835769
@@ -9,3 +15,4 @@ else
 	RS_DISABLE_A53_WORKAROUND := true
 endif
 
+APPLY_A53_ERRATA_FIXES :=
