9dc9020cb8a966cf23df93f9390b35355e65ae2a
Alex Naidis <alex.naidis@linux.com>
2017-07-30 03:29:42 +0000

    build: condition cortex-a53-835769 workaround for the linker
    Change-Id: I10734dd5aab2987b992299ebd4c92bab709c3a8a
    Signed-off-by: DarkKnight6499 <yazad.madan@gmail.com>

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -1,7 +1,9 @@
 ifneq (,$(filter cortex-a53,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
 	arch_variant_cflags := -mcpu=cortex-a53
+	arch_variant_ldflags += -Wl,--fix-cortex-a53-835769
 else
 	arch_variant_cflags :=
+	arch_variant_ldflags += -Wl,--no-fix-cortex-a53-835769
 	RS_DISABLE_A53_WORKAROUND := true
 endif
 
